53d2dfff8d606a2a146072f7d9a33f757b6e5d69
Shell2016 <bvg2003@bk.ru>
2018-03-26 15:06:13 +0300

    реализовано закрытие попапов по крестику

--- js/script.js
+++ js/script.js
@@ -4,32 +4,40 @@ var mapOpen = document.querySelector(".map");
 var map = document.querySelector(".map-popup");
 var cartOpen = document.querySelectorAll(".buy-btn");
 var cart = document.querySelector(".modal-cart")
-var close = document.querySelector(".modal-close");
-var modal = document.querySelector(".modal");
+var close = document.querySelectorAll(".modal-close");
+var modal = document.querySelectorAll(".modal");
 
 
 if (feedbackOpen) {
     feedbackOpen.addEventListener("click", function (event) {
-    event.preventDefault();
-    feedback.classList.add("modal-show");
-});
+        event.preventDefault();
+        feedback.classList.add("modal-show");
+    });
 }
-
 
 if (mapOpen) {
     mapOpen.addEventListener("click", function (event) {
-    event.preventDefault();
-    map.classList.add("modal-show");
-});
+        event.preventDefault();
+        map.classList.add("modal-show");
+    });
 }
-
 
 for (var i = 0; i < cartOpen.length; i++) {
     cartOpen[i].addEventListener("click", function (event) {
-    event.preventDefault();
-    cart.classList.add("modal-show");
-});
+        event.preventDefault();
+        cart.classList.add("modal-show");
+    });
 }
+
+for (var x = 0; x < close.length; x++) {
+    close[x].addEventListener("click", function (event) {
+        event.preventDefault();
+        for (var y = 0; y < modal.length; y++) {
+            modal[y].classList.remove("modal-show");
+        }
+    });
+}
+
 
 
 
