e2a3a389c1713dd52cd501f92d2d5fbdee09b91b
Shell2016 <bvg2003@bk.ru>
2018-03-26 14:33:10 +0300

    фикс открытия попапов

--- js/script.js
+++ js/script.js
@@ -2,25 +2,36 @@ var feedbackOpen = document.querySelector(".feedback-modal-open");
 var feedback = document.querySelector(".modal-feedback");
 var mapOpen = document.querySelector(".map");
 var map = document.querySelector(".map-popup");
-var cartOpen = document.querySelector(".buy-btn");
+var cartOpen = document.querySelectorAll(".buy-btn");
 var cart = document.querySelector(".modal-cart")
 var close = document.querySelector(".modal-close");
 var modal = document.querySelector(".modal");
 
 
-feedbackOpen.addEventListener("click", function (event) {
-  event.preventDefault();
-  feedback.classList.add("modal-show");
+if (feedbackOpen) {
+    feedbackOpen.addEventListener("click", function (event) {
+    event.preventDefault();
+    feedback.classList.add("modal-show");
 });
+}
 
-mapOpen.addEventListener("click", function (event) {
-  event.preventDefault();
-  map.classList.add("modal-show");
-});
 
-cartOpen.addEventListener("click", function (event) {
-  event.preventDefault();
-  cart.classList.add("modal-show");
+if (mapOpen) {
+    mapOpen.addEventListener("click", function (event) {
+    event.preventDefault();
+    map.classList.add("modal-show");
 });
+}
+
+
+for (var i = 0; i < cartOpen.length; i++) {
+    cartOpen[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    cart.classList.add("modal-show");
+});
+}
+
+
+
 
 
